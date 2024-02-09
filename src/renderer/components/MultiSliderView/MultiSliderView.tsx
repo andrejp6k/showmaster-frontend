a0310c465d9f2602a71c4d9443f915b55add280d
Matej Mikus <m.mikus@cid.com>
2024-02-09 13:50:48 +0100

    did some improvements to multisliderview

--- src/renderer/components/MultiSliderView/MultiSliderView.tsx
+++ src/renderer/components/MultiSliderView/MultiSliderView.tsx
@@ -3,56 +3,72 @@ import { Slider } from '@mui/material';
 interface MultiSliderViewProps {
   min: number;
   max: number;
-  teamValues?: number[];
-  correctValue?: number;
+  teamValues?: { flag: 'Team1' | 'Team2' | 'Correct'; value: number }[];
+  correctValue?: { flag: 'Team1' | 'Team2' | 'Correct'; value: number };
 }
 
 const MultiSLiderView: React.FC<MultiSliderViewProps> = ({ min, max, teamValues, correctValue }) => {
-  const allValues = [...(teamValues || []), ...(correctValue !== undefined ? [correctValue] : [])];
+  let allValues = [...(teamValues || []), ...(correctValue !== undefined ? [correctValue] : [])];
+  allValues = allValues.sort((a, b) => a.value - b.value);
+
+  function calculateTop(index: number) {
+    const flag = allValues[index]?.flag;
+
+    if (flag === 'Team1') {
+      return '60px';
+    }
+
+    if (flag === 'Team2') {
+      return '95px';
+    }
+
+    return '0px';
+  }
+
   return (
     <>
       <Slider
         track={false}
-        value={allValues}
-        valueLabelFormat={(asd, index) => (
-          <>
-            <div>{asd}</div>
-            <div>{index}</div>
-          </>
-        )}
+        value={allValues.map((x) => x.value)}
+        valueLabelFormat={(asd, index) => {
+          const flag = allValues[index].flag;
+          return (
+            <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+              <div>{asd}</div>
+              <div>{flag}</div>
+            </div>
+          );
+        }}
         min={min}
         max={max}
         step={1}
         disabled
         valueLabelDisplay="on"
         sx={{
+          mt: 3,
           '.MuiSlider-thumb': {
             ['&[data-index="0"]']: {
               background: 'red',
               '& .MuiSlider-valueLabel': {
-                top: 70,
+                top: calculateTop(0),
               },
             },
             ['&[data-index="1"]']: {
               background: 'green',
               '& .MuiSlider-valueLabel': {
-                top: 100,
+                top: calculateTop(1),
               },
             },
             ['&[data-index="2"]']: {
               background: 'blue',
               '& .MuiSlider-valueLabel': {
-                top: 130,
-                // background: 'green',
-                // borderRadius: '10px',
-                // opacity: '50^'
+                top: calculateTop(2),
               },
             },
             '& .MuiSlider-valueLabel': {
               position: 'fixed',
               fontSize: 12,
               fontWeight: 'normal',
-              // top: 70,
               backgroundColor: 'unset',
               color: 'black',
               '&::before': {
--- src/renderer/components/WelcomeHost/WelcomeHost.tsx
+++ src/renderer/components/WelcomeHost/WelcomeHost.tsx
@@ -2,6 +2,8 @@ import { useNavigate } from 'react-router-dom';
 import styles from './WelcomeHost.scss';
 import { RouteDefinitions } from '../../App';
 import Button from '../Button/Button';
+import { Slider } from '@mui/material';
+import MultiSLiderView from '../MultiSliderView/MultiSliderView';
 
 function WelcomeHost() {
   const navigate = useNavigate();
@@ -40,6 +42,15 @@ function WelcomeHost() {
         <Button onClick={handlePlayGame} sx={{ width: '250px' }}>
           Play game
         </Button>
+        <MultiSLiderView
+          min={1800}
+          max={2000}
+          teamValues={[
+            { flag: 'Team1', value: 1902 },
+            { flag: 'Team2', value: 1898 },
+          ]}
+          correctValue={{ flag: 'Correct', value: 1900 }}
+        />
       </div>
     </div>
   );
