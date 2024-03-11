fb16f12444fa646fc87499468d1f5be81c5c4363
d.podhorets <d.podhorets@cid.com>
2024-03-11 20:16:46 +0100

    implement change team score

--- src/renderer/components/GameSettings/GameSettings.tsx
+++ src/renderer/components/GameSettings/GameSettings.tsx
@@ -1,14 +1,15 @@
 import { Slider } from '@mui/material';
 import styles from './GameSettings.scss';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { selectGame } from '../../../redux/gameSlice';
 import { useAppDispatch, useAppSelector } from '../../hooks/appStore';
-import { selectShow, selectShowGame, setShowGame } from '../../../redux/showSlice';
+import { selectShow, selectShowGame, setShowGame, setTeamScores } from '../../../redux/showSlice';
 import { useNavigate } from 'react-router-dom';
 import { services } from '../../../services';
-import { UpdateShowGameRequest } from '../../../types';
+import { Score, UpdateShowGameRequest } from '../../../types';
 import Button from '../Button/Button';
+import TeamScore from './TeamScore/TeamScore';
 
 const DEFAULT_MAX_SCORE_TO_WIN = 30;
 
@@ -19,6 +20,10 @@ function GameSettings() {
   const [winningScore, setWinningScore] = useState(showGame?.scoreToWin || 0);
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
+  const [teamScoresSettings, setTeamScoresSettings] = React.useState<Score[]>([
+    { userId: showGame?.teamScores[0].userId, value: showGame?.teamScores[0].value },
+    { userId: showGame?.teamScores[1].userId, value: showGame?.teamScores[1].value },
+  ]);
 
   function handleWinningScoreChange(event: any) {
     setWinningScore(event.target.value);
@@ -30,11 +35,25 @@ function GameSettings() {
     try {
       const response = await services.shows.updateShowGame(show?.id!, showGame?.gameId!, updateRequest);
       dispatch(setShowGame(response.data));
-      navigate(-1);
     } catch (error) {
       // TODO: handle this
     }
+
+    if (teamScoresSettings[0].value != showGame?.teamScores[0].value || teamScoresSettings[1].value != showGame?.teamScores[1].value) {
+      try {
+        const response = await services.shows.modifyTeamScore(show?.id!, showGame?.gameId!, teamScoresSettings);
+        dispatch(setTeamScores({ gameId: showGame.gameId!, teamScores: response.data }));
+      } catch (error) {
+        // TODO: handle this
+      }
+    }
+
+    navigate(-1);
   }
+
+  const updateTeamScoreSettings = (userId: string, newScore: number) => {
+    setTeamScoresSettings((prevTeamScores) => prevTeamScores.map((scoreData) => (scoreData.userId === userId ? { ...scoreData, value: newScore } : scoreData)));
+  };
 
   return (
     <div className={styles.container}>
@@ -56,6 +75,14 @@ function GameSettings() {
             />
           </span>
         </span>
+        <div className={styles.score}>
+          <div className={styles.scoreLabel}>Teams score</div>
+          {teamScoresSettings.map((teamScoreSettings) => (
+            <div key={teamScoreSettings.userId} className={styles.teamScore}>
+              <TeamScore teamScoreSettings={teamScoreSettings} updateTeamScoreSettings={updateTeamScoreSettings}></TeamScore>
+            </div>
+          ))}
+        </div>
       </div>
       <div className={styles.footer}>
         <Button className={styles.submit} onClick={handleSaveClick}>
